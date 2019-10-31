092d23203ff433cc7246d60bd4c7755fd5670511
USER <USER@192.168.110.254>
2019-10-31 15:55:52 +0900

    구구단 추가욤

--- QTest/src/com/test/QTest.java
+++ QTest/src/com/test/QTest.java
@@ -5,6 +5,13 @@ public class QTest {
 		System.out.println("안녕 여러분");
 		
 		System.out.println("넴 넨");
+		
+		for(int i =2; i<=9; i++) {
+			for(int j=1; j<=9; j++) {
+				System.out.printf("%d * %d = %d\n", i,j,i*j);
+			}
+			System.out.println();
+		}
 	}
 
 }
