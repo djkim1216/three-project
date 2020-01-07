2bafb1aa01a4ec92b9716ccb29fb835c7963b5aa
djkim1216 <primarydj@daum.net>
2020-01-07 13:51:06 +0900

    prn추가!

--- QTest/src/com/test/QTest.java
+++ QTest/src/com/test/QTest.java
@@ -1,7 +1,8 @@
 package com.test;
 
 public class QTest {
-	public static void main(String[] args) {
+	
+	public static void prn() {
 		System.out.println("안녕 여러분");
 		
 		System.out.println("넴 넨");
@@ -13,6 +14,10 @@ public class QTest {
 			System.out.println();
 			System.out.println();
 		}
+	}
+	
+	public static void main(String[] args) {
+		prn();
 	}
 	/*
 	SQL
